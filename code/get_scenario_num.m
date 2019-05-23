ac3ed71ee30d4cd92eb464df6e2866dc503fe8b8
xb00dx <gengxinbo@gmail.com>
2019-05-23 16:51:02 -0500

    improve the get_scenario_num function: (1) using binary search; (2) compare two different implementations

--- code/get_scenario_num.m
+++ code/get_scenario_num.m
@@ -40,28 +40,28 @@ end
 switch opt
     case 'exact' % '2008' in [Campi2008], same as opt='exact'
         % #scenario cannot be smaller than (d-1)
-        n = d-1;
-        log_eps = log(epsilon); log_1_eps = log(1-epsilon);
-        while 1
-            aux = zeros(d,1);
-            aux(d) = (n-0)*log_1_eps;
-            for i = 1:(d -1)
-                % to be more accurate, taking log on nchoosek, then sum them
-                aux(i) = sum(log(1:n)) - sum(log(1:i)) - sum(log(1:(n-i)))...
-                    + i*log_eps + (n-i)*log_1_eps;
-            end
-            sum_res = sum( exp( aux ) );
-            if sum_res < beta
-               break; 
+        N_lb = d; N_ub = d;
+        beta_ub = 1;
+        % find the first N_ub such that beta_ub < beta
+        while (beta_ub > beta)
+            N_ub = N_ub * 2;
+%             N_ub = N_ub^2;
+            beta_ub = calculate_beta(epsilon, d, N_ub);
+        end
+        % then begin binary search
+        while (N_ub - N_lb) > 1
+            assert(N_ub > N_lb);
+            N_mid = floor( (N_ub + N_lb)/2 );
+            beta_mid = calculate_beta(epsilon, d, N_mid);
+            if beta_mid > beta
+                N_lb = N_mid;
             else
-                % increamental enumeration, could be very slow
-                % better using sth like binary search
-                n = n+1; 
+                N_ub = N_mid;
             end
         end
-        N = n;
+        N = N_ub;
     case 'fast' % '2009' in [Campi2009], same as 'fast'
-        N = 2/epsilon * (log(1/beta) + d);
+        N = ceil( 2/epsilon * (log(1/beta) + d) );
     case 's-and-d'
     case 'prior'
     case 'posterior' 
@@ -75,6 +75,44 @@ end
 end
 
 %% References
-% [Calafiore2006] Giuseppe C Calafiore, Marco C Campi, and others, “The scenario approach to robust control design,” IEEE Transactions on Automatic Control, 2006.
-% [Campi2008] Marco C Campi and Simone Garatti, “The exact feasibility of randomized solutions of uncertain convex programs,” SIAM Journal on Optimization, vol. 19, no. 3, pp. 1211–1230, 2008.
-% [Campi2009] M. C. Campi, S. Garatti, and M. Prandini, “The scenario approach for systems and control design,” Annual Reviews in Control, vol. 33, no. 2, pp. 149–157, 2009.
+% [Calafiore2006] Giuseppe C Calafiore, Marco C Campi, and others, ï¿½The scenario approach to robust control design,ï¿½ IEEE Transactions on Automatic Control, 2006.
+% [Campi2008] Marco C Campi and Simone Garatti, ï¿½The exact feasibility of randomized solutions of uncertain convex programs,ï¿½ SIAM Journal on Optimization, vol. 19, no. 3, pp. 1211ï¿½1230, 2008.
+% [Campi2009] M. C. Campi, S. Garatti, and M. Prandini, ï¿½The scenario approach for systems and control design,ï¿½ Annual Reviews in Control, vol. 33, no. 2, pp. 149ï¿½157, 2009.
+
+%% calculate_beta
+% Every iteration needs to calculate the value of beta
+% Two versions were implemented and compared
+% Parameters: d_in = 24, epsilon = 1e-4, beta = 1e-5
+% First version: 3.183780 seconds
+% Second version: 4.477356 seconds
+
+%% first verion, using for loops
+function beta_out = calculate_beta(eps_in, d_in, N_in)
+    log_eps = log(eps_in); log_1_eps = log(1-eps_in);
+    aux = zeros(d_in,1);
+    aux(d_in) = (N_in-0)*log_1_eps;
+    for i = 1:(d_in-1)
+        % to be more accurate, taking log on nchoosek, then sum them
+        aux(i) = sum(log(1:N_in)) - sum(log(1:i)) - sum(log(1:(N_in-i)))...
+            + i*log_eps + (N_in-i)*log_1_eps;
+    end
+    beta_out = sum( exp(aux) );
+end
+
+%% second verion, avoiding for loops by matrix operation
+% function beta_out = calculate_beta(eps_in, d_in, N_in)
+%     log_eps = log(eps_in); log_1_eps = log(1-eps_in);
+%     % aux1 is a column vector: log( N!/0!) ... log( N!/(d-1)!)
+%     m1 = 1:1:N_in; assert(size(m1,1) == 1);
+%     aux1 = sum( triu( log(ones(d_in,1)*m1) ) ,2);
+%     % aux2 is a column vector: log( 0!) ... log( (d-1)!)
+%     m2 = N_in:(-1):1; assert(size(m2,1) == 1);
+%     aux2 = sum( triu( log(ones(d_in,1)*m2) ), 2);
+% %     coeffs = aux1-aux2;
+%     % for checking
+% %     coeffs = exp( aux1-aux2 );
+% %     aa = exp(aux1); bb = exp( aux2 ); open aa, open bb;
+%     aux = aux1 - aux2 + (0:1:(d_in-1))'*log_eps + (N_in:(-1):(N_in-d_in+1))'*log_1_eps;
+%     assert(size(aux,2) == 1);
+%     beta_out = sum( exp(aux) );
+% end
